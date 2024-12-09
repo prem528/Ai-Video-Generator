3ee5e31383d7beb0bb6ccf8ae945d4e227733e45
prem528 <prem05082002@gmail.com>
2024-12-10 01:11:11 +0530

    first commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,19 @@
+import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
+import login from '../../../../public/login.jpg'
+
+export default function Page() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2'>
+      <div>
+        <Image src={login} 
+        alt='login' width={500} height={500} 
+        className='w-full h-screen object-contain'
+        />
+      </div>
+      <div className='flex justify-center items-center h-screen'>
+        <SignIn />
+      </div>
+    </div>
+  )
+}
